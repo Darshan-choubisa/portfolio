811edafc2f38861614dff5661967eaf31bd09d16
Darshan-choubisa <darshanchoubisa59@gmail.com>
2025-10-14 21:51:09 +0530

    change code

--- style.js
+++ style.js
@@ -3,10 +3,10 @@ const card2 = document.getElementById("card-2");
 const card3 = document.getElementById("card-3");
 const card4 = document.getElementById("card-4");
 
-const Aboutme = document.getElementById("aboutme-link");
-const Summary = document.getElementById("summary-link");
-const Port = document.getElementById("portfolio-link");
-const Contact = document.getElementById("contact-link");
+// const Aboutme = document.getElementById("aboutme-link");
+// const Summary = document.getElementById("summary-link");
+// const Port = document.getElementById("portfolio-link");
+// const Contact = document.getElementById("contact-link");
 
 function showOnly(section) {
   card1.classList.add("hidden");
@@ -62,41 +62,133 @@ document.querySelectorAll(".contact-link").forEach(Contact => {
     showOnly(card4);
   });
 })
- 
 
+// Drop down
+const dropdownBtn = document.getElementById("dropdownBtn");
+const dropdownMenu = document.getElementById("dropdownMenu");
 
-// hidden box 
-// const toggleBtn = document.getElementById("toggleBtn");
-// const detailsBox = document.getElementById("detailsBox");
-
-// toggleBtn.addEventListener("click", () => {
-//   detailsBox.classList.toggle("hidden");
-//   toggleBtn.textContent = detailsBox.classList.contains("hidden")
-//     ? "More"
-//     : "Less";
-// });
-;
-
-
-const toggleBtn2 = document.getElementById("toggleBtn2");
-const detailsBox = document.getElementById("detailsBox");
-const maincontainer = document.getElementById("main-container")
-
-toggleBtn2.addEventListener("click", () => {
-  const isHidden = detailsBox.classList.contains("max-h-0");
+dropdownBtn.addEventListener("click", () => {
+  const isHidden = dropdownMenu.classList.contains("opacity-0");
 
   if (isHidden) {
-    detailsBox.classList.remove("max-h-0", "opacity-0");
-    detailsBox.classList.add("max-h-[500px]", "opacity-100");
-    detailsBox.style.margintop = "8px"
-    maincontainer.style.marginTop = "8px"
-    toggleBtn2.textContent = "Less";
+    // show menu
+    dropdownMenu.classList.remove("opacity-0", "scale-95", "pointer-events-none");
+    dropdownMenu.classList.add("opacity-100", "scale-100");
   } else {
-    detailsBox.classList.add("max-h-0", "opacity-0");
-    detailsBox.classList.remove("max-h-[500px]", "opacity-100");
-    toggleBtn2.textContent = "More";
+    // hide menu
+    dropdownMenu.classList.add("opacity-0", "scale-95", "pointer-events-none");
+    dropdownMenu.classList.remove("opacity-100", "scale-100");
   }
 });
+
+
+const project1 = document.getElementById("project-1");
+const project2 = document.getElementById("project-2");
+const project3 = document.getElementById("project-3");
+const project4 = document.getElementById("project-4");
+
+
+const all = document.querySelectorAll(".all");
+const webdesign = document.querySelectorAll(".web-design");
+const design = document.querySelectorAll(".design");
+const webdevelopment = document.querySelectorAll(".web-development")
+
+// function showproject(project) {
+//   project1.classList.remove("hidden")
+//   project2.classList.remove("hidden")
+//   project3.classList.remove("hidden")
+//   project4.classList.remove("hidden")
+
+//   project.classList.add("hidden")
+// }
+
+document.querySelectorAll(".all").forEach(all => {
+  all.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (classList == hidden) {
+      project1.classList.remove("hidden")
+      project2.classList.remove("hidden")
+      project3.classList.remove("hidden")
+      project4.classList.remove("hidden")
+    }
+    else {
+      project2.classList.add("hidden")
+      project1.classList.add("hidden")
+      project3.classList.add("hidden")
+      project4.classList.add("hidden")
+    }
+  })
+})
+
+document.querySelectorAll(".web-design").forEach(webdesign => {
+  webdesign.addEventListener("click", (e) => {
+    e.preventDefault()
+    showproject(project1)
+    showproject(project2)
+  })
+})
+
+document.querySelectorAll(".design").forEach(design => {
+  design.addEventListener("click", (e) => {
+    e.preventDefault()
+    showproject(project3)
+  })
+})
+
+document.querySelectorAll(".web-development").forEach(webdevelopment => {
+  webdevelopment.addEventListener("click", (e) => {
+    e.preventDefault()
+    showproject(project4)
+  })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // hidden box 
+  // const toggleBtn = document.getElementById("toggleBtn");
+  // const detailsBox = document.getElementById("detailsBox");
+
+  // toggleBtn.addEventListener("click", () => {
+  //   detailsBox.classList.toggle("hidden");
+  //   toggleBtn.textContent = detailsBox.classList.contains("hidden")
+  //     ? "More"
+  //     : "Less";
+  // });
+  ;
+
+
+// const toggleBtn2 = document.getElementById("toggleBtn2");
+// const detailsBox = document.getElementById("detailsBox");
+// const maincontainer = document.getElementById("main-container")
+
+// toggleBtn2.addEventListener("click", () => {
+//   const isHidden = detailsBox.classList.contains("max-h-0");
+
+//   if (isHidden) {
+//     detailsBox.classList.remove("max-h-0", "opacity-0");
+//     detailsBox.classList.add("max-h-[500px]", "opacity-100");
+//     detailsBox.style.margintop = "8px"
+//     maincontainer.style.marginTop = "8px"
+//     toggleBtn2.textContent = "Less";
+//   } else {
+//     detailsBox.classList.add("max-h-0", "opacity-0");
+//     detailsBox.classList.remove("max-h-[500px]", "opacity-100");
+//     toggleBtn2.textContent = "More";
+//   }
+// });
 
 
 
@@ -106,15 +198,16 @@ toggleBtn2.addEventListener("click", () => {
 
 
 
-// sidebar-menu 
-const menuToggle = document.getElementById("menuToggle");
-const sidebar = document.getElementById("sidebar");
+// sidebar-menu
+// const menuToggle = document.getElementById("menuToggle");
+// const sidebar = document.getElementById("sidebar");
 
 
-menuToggle.addEventListener("click", () => {
-  sidebar.classList.toggle("-translate-x-full");
-  sidebar.classList.toggle("translate-x-0");
-});
+// menuToggle.addEventListener("click", () => {
+//   sidebar.classList.toggle("-translate-x-full");
+//   sidebar.classList.toggle("translate-x-0");
+// });
+
 
 
 
