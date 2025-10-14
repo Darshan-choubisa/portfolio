ff1f24aab725ef3481983401eb7155350bc3ab94
Darshan-choubisa <darshanchoubisa59@gmail.com>
2025-10-14 20:22:10 +0530

    change code

--- style.js
+++ style.js
@@ -3,10 +3,10 @@ const card2 = document.getElementById("card-2");
 const card3 = document.getElementById("card-3");
 const card4 = document.getElementById("card-4");
 
-const Aboutme = document.getElementById("aboutme");
-const Summary = document.getElementById("summary");
-const Port = document.getElementById("port");
-const Contact = document.getElementById("contact");
+const Aboutme = document.getElementById("aboutme-link");
+const Summary = document.getElementById("summary-link");
+const Port = document.getElementById("portfolio-link");
+const Contact = document.getElementById("contact-link");
 
 function showOnly(section) {
   card1.classList.add("hidden");
@@ -17,70 +17,106 @@ function showOnly(section) {
   section.classList.remove("hidden");
 }
 
-Aboutme.addEventListener("click", (e) => {
-  e.preventDefault();
-  showOnly(card1);
-});
-Summary.addEventListener("click", (e) => {
-  e.preventDefault();
-  showOnly(card2);
-});
-Port.addEventListener("click", (e) => {
-  e.preventDefault();
-  showOnly(card3);
-});
-Contact.addEventListener("click", (e) => {
-  e.preventDefault();
-  showOnly(card4);
-});
-
-const toggleBtn = document.getElementById("toggleBtn");
-    const detailsBox = document.getElementById("detailsBox");
-
-    toggleBtn.addEventListener("click", () => {
-      detailsBox.classList.toggle("hidden");
-      toggleBtn.textContent = detailsBox.classList.contains("hidden")
-        ? "More"
-        : "Less";
-    });
-
-// // By default show About section
-// main1.classList.remove("hidden");
-// main2.classList.add("hidden");
-// main3.classList.add("hidden");
-// main4.classList.add("hidden");
-
-// // About me
-// Aboutme.addEventListener("click", () => {
-//     main1.classList.remove("hidden");
-//     main2.classList.add("hidden");
-//     main3.classList.add("hidden");
-//     main4.classList.add("hidden");
+// Aboutme.addEventListener("click", (e) => {
+//   e.preventDefault();
+//   showOnly(card1);
 // });
 
-// // Summary
-// Summary.addEventListener("click", () => {
-//     main1.classList.add("hidden");
-//     main2.classList.remove("hidden");
-//     main3.classList.add("hidden");
-//     main4.classList.add("hidden");
+// Summary.addEventListener("click", (e) => {
+//   e.preventDefault();
+//   showOnly(card2);
+// });
+// Port.addEventListener("click", (e) => {
+//   e.preventDefault();
+//   showOnly(card3);
+// });
+// Contact.addEventListener("click", (e) => {
+//   e.preventDefault();
+//   showOnly(card4);
 // });
 
-// // Portfolio
-// Port.addEventListener("click", () => {
-//     main1.classList.add("hidden");
-//     main2.classList.add("hidden");
-//     main3.classList.remove("hidden");
-//     main4.classList.add("hidden");
-// });
 
-// // Contact
-// Contact.addEventListener("click", () => {
-//     main1.classList.add("hidden");
-//     main2.classList.add("hidden");
-//     main3.classList.add("hidden");
-//     main4.classList.remove("hidden");
+document.querySelectorAll(".aboutme-link").forEach(Aboutme => {
+  Aboutme.addEventListener("click", (e) => {
+    e.preventDefault();
+    showOnly(card1);
+  });
+})
+
+document.querySelectorAll(".summary-link").forEach(Summary => {
+  Summary.addEventListener("click", (e) => {
+    e.preventDefault();
+    showOnly(card2);
+  });
+})
+
+document.querySelectorAll(".portfolio-link").forEach(Port => {
+  Port.addEventListener("click", (e) => {
+    e.preventDefault();
+    showOnly(card3);
+  });
+})
+document.querySelectorAll(".contact-link").forEach(Contact => {
+  Contact.addEventListener("click", (e) => {
+    e.preventDefault();
+    showOnly(card4);
+  });
+})
+ 
+
+
+// hidden box 
+// const toggleBtn = document.getElementById("toggleBtn");
+// const detailsBox = document.getElementById("detailsBox");
+
+// toggleBtn.addEventListener("click", () => {
+//   detailsBox.classList.toggle("hidden");
+//   toggleBtn.textContent = detailsBox.classList.contains("hidden")
+//     ? "More"
+//     : "Less";
 // });
+;
+
+
+const toggleBtn2 = document.getElementById("toggleBtn2");
+const detailsBox = document.getElementById("detailsBox");
+const maincontainer = document.getElementById("main-container")
+
+toggleBtn2.addEventListener("click", () => {
+  const isHidden = detailsBox.classList.contains("max-h-0");
+
+  if (isHidden) {
+    detailsBox.classList.remove("max-h-0", "opacity-0");
+    detailsBox.classList.add("max-h-[500px]", "opacity-100");
+    maincontainer.style.marginTop = "8px"
+    toggleBtn2.textContent = "Less";
+  } else {
+    detailsBox.classList.add("max-h-0", "opacity-0");
+    detailsBox.classList.remove("max-h-[500px]", "opacity-100");
+    toggleBtn2.textContent = "More";
+  }
+});
+
+
+
+// window.addEventListener("load", () => {
+//   detailsBox.classList.remove("hidden");
+// })
+
+
+
+// sidebar-menu 
+const menuToggle = document.getElementById("menuToggle");
+const sidebar = document.getElementById("sidebar");
+
+
+menuToggle.addEventListener("click", () => {
+  sidebar.classList.toggle("-translate-x-full");
+  sidebar.classList.toggle("translate-x-0");
+});
+
+
+
 
 
 // const sections = {
@@ -116,10 +152,9 @@ const toggleBtn = document.getElementById("toggleBtn");
 
 
 // const menuToggle = document.getElementById("menuToggle");
-// const sidebar = document.getElementById("aboutme-desktop");
-// const contentWrapper = document.getElementById("contentWrapper");
+// const sidebar = document.getElementById("sidebar");
 
-// let menuOpen = false;
+// // let menuOpen = false;
 
 // menuToggle.addEventListener("click", () => {
 //   menuOpen = !menuOpen;
@@ -136,6 +171,9 @@ const toggleBtn = document.getElementById("toggleBtn");
 //     contentWrapper.classList.remove("ml-64");
 //   }
 // });
+
+
+
 
 
 
